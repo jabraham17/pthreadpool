cd7a0d6f6a62d1cc35a3fd913046c493bc322de4
Jacob Abraham <jacob.r.abraham@gmail.com>
2021-04-20 12:38:52 -0700

    split timespec helper into 2 files

--- timespec_helper.h
+++ timespec_helper.h
@@ -5,56 +5,23 @@
 #include <time.h>
 
 // given an amount in s and ns, place the values in the timespec
-void th_calc_timespec(struct timespec *ts, unsigned long long s,
-                      unsigned long long ns) {
-    // we have to make sure we dont execde integer limits
-    // or that we dont excede GNUC specs
-    while(ns >= 1000ull * 1000ull * 1000ull) {
-        s += 1;
-        ns -= 1000ull * 1000ull * 1000ull;
-    }
-    ts->tv_sec = s;
-    ts->tv_nsec = ns;
-}
+void th_calc_timespec(struct timespec* ts, unsigned long long s,
+                      unsigned long long ns);
 // convert timespec to ns
-unsigned long long th_conv_timespec_ns(struct timespec *ts) {
-    return ts->tv_nsec +
-           (unsigned long long)ts->tv_sec * 1000ull * 1000ull * 1000ull;
-}
+unsigned long long th_conv_timespec_ns(struct timespec* ts);
 // convert timespec to us
-float th_conv_timespec_us(struct timespec *ts) {
-    return (float)ts->tv_nsec / 1000.0f + (float)ts->tv_sec * 1000.0f * 1000.0f;
-}
+float th_conv_timespec_us(struct timespec* ts);
 // convert timespec to ms
-float th_conv_timespec_ms(struct timespec *ts) {
-    return (float)ts->tv_nsec / 1000.0f / 1000.0f + (float)ts->tv_sec * 1000.0f;
-}
+float th_conv_timespec_ms(struct timespec* ts);
 // convert timespec to s
-float th_conv_timespec_s(struct timespec *ts) {
-    return (float)ts->tv_nsec / 1000.0f / 1000.0f / 1000.0f + (float)ts->tv_sec;
-}
+float th_conv_timespec_s(struct timespec* ts);
 
 // a - b
-void th_sub_timespec(struct timespec *ts, struct timespec *a,
-                     struct timespec *b) {
-    unsigned long long diff = th_conv_timespec_ns(a) - th_conv_timespec_ns(b);
-    th_calc_timespec(ts, 0, diff);
-}
-void th_add_timespec(struct timespec *ts, struct timespec *a,
-                     struct timespec *b) {
-    unsigned long long sum = th_conv_timespec_ns(a) + th_conv_timespec_ns(b);
-    th_calc_timespec(ts, 0, sum);
-}
+void th_sub_timespec(struct timespec* ts, struct timespec* a,
+                     struct timespec* b);
+void th_add_timespec(struct timespec* ts, struct timespec* a,
+                     struct timespec* b);
 
-unsigned long long th_gettime_ns() {
-    struct timespec t;
-    clock_gettime(CLOCK_MONOTONIC, &t);
-    return th_conv_timespec_ns(&t);
-}
-float th_gettime_ms() {
-    struct timespec t;
-    clock_gettime(CLOCK_MONOTONIC, &t);
-    return th_conv_timespec_ms(&t);
-}
-
+unsigned long long th_gettime_ns();
+float th_gettime_ms();
 #endif
